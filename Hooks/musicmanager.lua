338f533ab75810abb9818a5c1574f8837c48b568
Xeletron <xeletron1@gmail.com>
2023-09-17 19:15:29 +0200

    Fixed music

--- Hooks/musicmanager.lua
+++ Hooks/musicmanager.lua
@@ -1,3 +1,3 @@
 Hooks:PostHook(MusicManager, "jukebox_default_tracks", "RogueCompany_default_track", function(self)
-    self:track_attachment_add("heist_RogueCompany_name", "track_fk")
+    self:track_attachment_add("heist_RogueCompanyNarr_name", "track_fk")
 end)
